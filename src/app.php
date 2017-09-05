b4c5794d54868e55b0cce7181bec5f6712df6ce5
maqmaq <m.mmackows@gmail.com>
2017-09-05 21:39:22 +0200

    installation of pdf generator, sample form

--- src/app.php
+++ src/app.php
@@ -1,16 +1,41 @@
 <?php
 
+define ('HOME_PATH', realpath(__DIR__ . '/../'));
+define ('VENDOR_PATH', realpath(HOME_PATH . '/vendor'));
+
+use Nass600\Silex\Provider\SnappyServiceProvider;
 use Silex\Application;
 use Silex\Provider\AssetServiceProvider;
 use Silex\Provider\TwigServiceProvider;
 use Silex\Provider\ServiceControllerServiceProvider;
 use Silex\Provider\HttpFragmentServiceProvider;
+use Silex\Provider\FormServiceProvider;
 
 $app = new Application();
 $app->register(new ServiceControllerServiceProvider());
 $app->register(new AssetServiceProvider());
 $app->register(new TwigServiceProvider());
 $app->register(new HttpFragmentServiceProvider());
+$app->register(new FormServiceProvider());
+$app->register(new Silex\Provider\TranslationServiceProvider(), array(
+    'translator.domains' => array(),
+    'locale_fallbacks' => array('en'),
+
+));
+$app->register(new Silex\Provider\LocaleServiceProvider());
+$app->register(new Silex\Provider\ValidatorServiceProvider());
+
+$app->register(new SnappyServiceProvider(), array(
+    'snappy.pdf.binary' => sprintf('%s/%s', VENDOR_PATH, 'bin/wkhtmltopdf-amd64'),
+    'snappy.pdf.options' => array(
+        'footer-center' => 'page [page]'
+    ),
+    'snappy.image.binary' => '/path/to/wkhtmltoimage',
+    'snappy.image.options' => array(
+        'format' => 'png'
+    )
+));
+
 $app['twig'] = $app->extend('twig', function ($twig, $app) {
     // add custom globals, filters, tags, ...
 
--- src/controllers.php
+++ src/controllers.php
@@ -5,13 +5,41 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Validator\Constraints as Assert;
 
 //Request::setTrustedProxies(array('127.0.0.1'));
 
-$app->get('/', function () use ($app) {
-    return $app['twig']->render('index.html.twig', array());
-})
-->bind('homepage')
+$app->match('/', function (Request $request) use ($app) {
+    // some default data for when the form is displayed the first time
+    $data = array(
+        'name' => 'Your name',
+    );
+
+    $form = $app['form.factory']->createBuilder(FormType::class, $data)
+        ->add('name', TextType::class, array(
+            'constraints' => array(new Assert\NotBlank(), new Assert\Length(array('min' => 5)))
+        ))
+        ->add('submit', SubmitType::class, [
+            'label' => 'Save',
+        ])
+        ->getForm();
+
+    $form->handleRequest($request);
+
+    if ($form->isValid()) {
+        $data = $form->getData();
+
+        $app['snappy.pdf']->generateFromHtml('<h1>Bill</h1><p>You owe me money, dude.</p>', '/tmp/output.pdf');
+        die();
+    }
+
+    // display the form
+    return $app['twig']->render('index.html.twig', array('form' => $form->createView()));
+})->bind('homepage')
 ;
 
 $app->error(function (\Exception $e, Request $request, $code) use ($app) {
